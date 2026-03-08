eb3ddbfe94d0e24c84cc98c3f354840d71566eff
Cynthia Tristán <cyntrist@ucm.es>
2026-03-08 04:33:22 +0100

    eleccion funciona

--- scripts/random_scene.gd
+++ scripts/random_scene.gd
@@ -1,7 +1,10 @@
 extends Scene
 
-@onready var random_bug_root : Node = $Bugsnax
-@onready var random_bug_sprite : Node = $Bugsnax/Random
+@onready var random_bug_root : Node2D = $Bugsnax
+@onready var random_bug_sprite : AnimatedSprite2D = $Bugsnax/Random
+@onready var chosen_sprite : Sprite2D = $Bugsnax/Chosen
+@onready var chosen_text : Label = $Texto/Label
+@onready var chosen_text_node : Control = $Texto
 
 @export var random_duration :float = 1.0 
 @export var random_scale :Vector2 = Vector2(1.5,1.5) 
@@ -21,6 +24,22 @@ func on_enable() -> void:
 	tween.set_trans(Tween.TRANS_SINE)
 	tween.set_ease(Tween.EASE_OUT)
 	tween.tween_property(random_bug_root, "scale", random_scale, random_duration)
+	tween.finished.connect(choose)
+	pass
+	
+func format_name(key: String) -> String:
+	var text = key.replace("-", " ")
+	text = text.capitalize()
+#	text = text.replace(" ", "\n")
+	return text	
+
+func choose():
+	var key : String  = Global.bugsnaxs.keys().pick_random()
+	var value : Texture2D = Global.bugsnaxs[key] # el sprite
+
+	chosen_sprite.texture = value
+	chosen_text_node.visible = true
+	chosen_text.text = format_name(key)
 	pass
 	
 func on_disable() -> void:
